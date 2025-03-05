daa8acb40c87e79fc33fe30ede8d47cc023ae939
刘帅洋 <liushuaiyang@aliyun.com>
2025-03-05 12:20:15 +0800

    fix: 修复部分章节路由不正确bug

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -9,36 +9,36 @@ import {
 } from "./chunk-LW4I4DCF.js";
 
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import { default as default3 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
-import { default as default4 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
-import { default as default5 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
-import { default as default6 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default7 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default8 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default9 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
-import { default as default10 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default11 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
-import { default as default12 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
-import { default as default13 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
-import { default as default14 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default15 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default16 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default17 } from "/Users/liushuaiyang/Desktop/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/icons.css";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import { default as default3 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPImage.vue";
+import { default as default4 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPButton.vue";
+import { default as default5 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeContent.vue";
+import { default as default6 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default7 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default8 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default9 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPLink.vue";
+import { default as default10 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default11 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPSocialLink.vue";
+import { default as default12 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPSocialLinks.vue";
+import { default as default13 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPSponsors.vue";
+import { default as default14 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default15 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default16 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default17 } from "/Users/liushuaiyang/个人项目/nest-docs/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 
 // node_modules/vitepress/dist/client/theme-default/support/utils.js
 import { withBase } from "vitepress";
